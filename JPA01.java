ed4e7bf6d848662e09905c025ac7f6bdd7f31e9c
sbbbbb76 <160613391+sbbbbb76@users.noreply.github.com>
2024-03-04 19:29:06 +0800

    Add files via upload

---
+++ JPA01.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class JPA01 {
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        for(int x=1 ; x<=2 ; x++){
+            System.out.println("Input:");
+            int x1 = a.nextInt();
+            if (x1 >= 60){
+                System.out.println("You pass");
+            }
+                System.out.println("The end");
+            
+        }
+        
+    }
+    
+}
